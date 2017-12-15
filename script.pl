35607c4f1c23ae2dd607a6a4d9cfbacd685699d7
Szymon Halastra <szymon.halastra@gmail.com>
2017-12-15 15:13:22 +0100

    Update script

--- script.pl
+++ script.pl
@@ -9,17 +9,18 @@ use Getopt::Long;
 
 {
     ## try to load Tie::IxHash
-    my $ordered_hash_available = eval { require Tie::IxHash };
+    my $ordered_hash = eval {require Tie::IxHash};
 
-    my $show_help = 0;
     my $write = "json";
-    my $trim_whitespaces = 0;
+
+    my $trim = 0;
+    my $display_help = 0;
 
     GetOptions(
-        "--help|h!" => \$show_help,
-        "--trim-whitespaces!" => \$trim_whitespaces,
+        "--help|h!"           => \$display_help,
+        "--trim!" => \$trim,
     ) or show_help();
-    show_help() if $show_help;
+    show_help() if $display_help;
 
     ## use STDIN if file name is not specified
     my ($fn) = @ARGV;
@@ -27,14 +28,9 @@ use Getopt::Long;
         die "input file name is required";
     }
 
-    my ($data, $format) = smart_read_data($fn, $ordered_hash_available, $trim_whitespaces);
+    my ($data, $format) = read_data($fn, $ordered_hash, $trim);
 
     my $output = \*STDOUT;
-    if ($write eq 'self') {
-        open($output, ">", $fn) or die "can't write [$fn]: $!";
-    } else {
-        $format = $write;
-    }
 
     write_json($data, $output);
 
@@ -44,42 +40,19 @@ use Getopt::Long;
 }
 
 sub show_help {
-    print "This script gives an user possibility to convert CSV to JSON. \n".
-        "To run execute this script and as an argument pass the name of csv file, i.e.:\n\n".
-        "   ./script SacramentocrimeJanuary2006.csv \n\n".
-        "As an output, converted JSON will be printed.\n".
-        "To save to new file, simply use tee command, i.e.:\n\n".
-        "   ./script SacramentocrimeJanuary2006.csv | tee <filename>.json\n\n".
-        "Additional arguments:\n".
-        "[--trim-whitespaces] - removes whitespaces from an output.\n".
-        "[--help, -h] - prints this help.\n".
+    print "This script gives an user possibility to convert CSV to JSON. \n" .
+        "To run execute this script and as an argument pass the name of csv file, i.e.:\n\n" .
+        "   ./script SacramentocrimeJanuary2006.csv \n\n" .
+        "As an output, converted JSON will be printed.\n" .
+        "To save to new file, simply use tee command, i.e.:\n\n" .
+        "   ./script SacramentocrimeJanuary2006.csv | tee <filename>.json\n\n" .
+        "Additional arguments:\n" .
+        "[--trim] - removes whitespaces from an output.\n" .
+        "[--help, -h] - prints this help.\n" .
         "\n";
     exit(1);
 }
 
-sub smart_read_data {
-    my ($fn, $ordered_hash_available, $trim_whitespaces) = @_;
-
-    my ($type_detected, $separator_detected) = smart_file_type_detection($fn);
-    unless (defined $type_detected) {
-        $type_detected = 'csv';
-    }
-    $separator_detected = ",";
-
-
-    my $data = undef;
-    if ($type_detected eq 'csv') {
-        $data = read_csv($fn, $ordered_hash_available, $separator_detected, $trim_whitespaces);
-    } elsif ($type_detected eq 'table') {
-        $data = read_table($fn, $ordered_hash_available, $trim_whitespaces);
-    } elsif ($type_detected eq 'xlsx') {
-        $data = read_xlsx($fn, $ordered_hash_available);
-    }
-    unless (defined $data) {
-        die "can't read $type_detected format from [$fn]";
-    }
-    return ($data, $type_detected);
-}
 
 sub smart_file_type_detection {
     my ($file) = @_;
@@ -90,13 +63,14 @@ sub smart_file_type_detection {
     my $count = 5;
     while (<$fh>) {
         $lines .= $_;
-        last unless -- $count;
+        last unless --$count;
     }
     close($fh);
 
-    if ($lines =~ m/^\s*[{\]]/) { ## "[" or "{"
+    if ($lines =~ m/^\s*[{\]]/) {## "[" or "{"
         return ('json');
-    } else {
+    }
+    else {
         my ($first_line, $second_line, undef) = split(m{\r?\n}, $lines, 3);
 
         if (split(",", $first_line) == split(",", $second_line)) {
@@ -106,41 +80,55 @@ sub smart_file_type_detection {
     return;
 }
 
-sub write_json {
-    my ($data, $output) = @_;
+sub read_data {
+    my ($fn, $ordered_hash, $trim_whitespaces) = @_;
 
-    print $output to_json(
-        $data,
-        {
-            'utf8' => 1,
-            'pretty' => 1,
-        }
-    );
+    my ($type_detected, $separator_detected) = smart_file_type_detection($fn);
+    unless (defined $type_detected) {
+        $type_detected = 'csv';
+    }
+    $separator_detected = ",";
+
+    my $data = undef;
+    if ($type_detected eq 'csv') {
+        $data = read_csv($fn, $ordered_hash, $separator_detected, $trim_whitespaces);
+    }
+    elsif ($type_detected eq 'table') {
+        $data = read_table($fn, $ordered_hash, $trim_whitespaces);
+    }
+    elsif ($type_detected eq 'xlsx') {
+        $data = read_xlsx($fn, $ordered_hash);
+    }
+    unless (defined $data) {
+        die "can't read $type_detected format from [$fn]";
+    }
+    return ($data, $type_detected);
 }
 
 sub read_csv {
-    my ($fn, $ordered_hash_available, $separator, $trim_whitespaces) = @_;
+    my ($fn, $ordered_hash, $separator, $trim_whitespaces) = @_;
 
     my $csv = Text::CSV->new(
-        {
-            'binary' => 1,
-            'sep_char' => $separator,
-            'allow_whitespace' => $trim_whitespaces,
-        }
-    ) or die "can't create Text::CSV: ".Text::CSV->error_diag();
+    {
+        'binary'           => 1,
+        'sep_char'         => $separator,
+        'allow_whitespace' => $trim_whitespaces,
+    }
+    ) or die "can't create Text::CSV: " . Text::CSV->error_diag();
 
     open(my $fh, "<", $fn) or die "can't read [$fn]: $!";
     binmode($fh, ":utf8");
     my ($columns, @rows);
-    while ( my $row = $csv->getline( $fh ) ) {
+    while (my $row = $csv->getline($fh)) {
         if (defined $columns) {
             my %r;
-            if ($ordered_hash_available) {
+            if ($ordered_hash) {
                 tie %r, "Tie::IxHash";
             }
             @r{@$columns} = @$row;
             push(@rows, \%r);
-        } else {
+        }
+        else {
             $columns = $row;
         }
     }
@@ -148,4 +136,16 @@ sub read_csv {
     close($fh);
 
     return \@rows;
+}
+
+sub write_json {
+    my ($data, $output) = @_;
+
+    print $output to_json(
+        $data,
+        {
+            'utf8'   => 1,
+            'pretty' => 1,
+        }
+    );
 }
