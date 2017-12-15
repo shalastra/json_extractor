015fcb7a3045aa6dbbfd6228567ad5774e15e10b
Szymon Halastra <szymon.halastra@gmail.com>
2017-12-15 13:45:45 +0100

    Update code, add example csv

--- script.pl
+++ script.pl
@@ -1,45 +1,145 @@
 #!/usr/bin/perl
 
-## IMPORTS
-use autodie;
 use strict;
-use utf8;
-use warnings qw(all);
+use warnings;
 
+use JSON;
+use Text::CSV;
 use Getopt::Long;
-use Pod::Usage;
 
-# HELP
+{
+    ## try to load Tie::IxHash
+    my $ordered_hash_available = eval { require Tie::IxHash };
+    my $xslx_format_available = eval { require Spreadsheet::XLSX };
 
-=head1 SYNOPSIS
+    my $show_help = 0;
+    my $write = "json";
+    my $trim_whitespaces = 0;
 
-    Script takes one file as a first argument. Next arguments are names
-    which should be extracted, i.e.:
-            perl script.pl <filename> <arg1>...<argN>
+    GetOptions(
+        "--help|h!" => \$show_help,
+        "--write=s" => \$write,
+        "--trim-whitespaces!" => \$trim_whitespaces,
+    ) or show_help();
+    show_help() if $show_help;
 
-=head1 DESCRIPTION
-
-...
-
-=cut
-
-GetOptions(
-    q(help)             => \my $help
-);
-pod2usage(q(-verbose) => 1) if $help;
-
-# JSON operation
-
-my $lineno = 1;
-my $current = "";
-
-while(<>) {
-    if($current ne $ARGV) {
-        $current = $ARGV;
-        print "\n\t\tFile: $ARGV\n\n";
-        $lineno = 1;
+    ## use STDIN if file name is not specified
+    my ($fn) = @ARGV;
+    unless (defined $fn) {
+        die "input file name is required";
     }
 
-    print $lineno++;
-    print ": $_";
+    my ($data, $format) = smart_read_data($fn, $ordered_hash_available, $trim_whitespaces, $xslx_format_available);
+
+    my $output = \*STDOUT;
+    if ($write eq 'self') {
+        open($output, ">", $fn) or die "can't write [$fn]: $!";
+    } else {
+        $format = $write;
+    }
+
+    write_json($data, $output);
+
+    if ($write eq 'self') {
+        close($output);
+    }
+}
+
+sub show_help {
+    print "Usage: $0 [--write=json] [--trim-whitespaces] ".
+        "[--help] <file>\n";
+    exit(1);
+}
+
+sub smart_read_data {
+    my ($fn, $ordered_hash_available, $trim_whitespaces, $xslx_format_available) = @_;
+
+    my ($type_detected, $separator_detected) = smart_file_type_detection($fn, $xslx_format_available);
+    unless (defined $type_detected) {
+        $type_detected = 'csv';
+    }
+    $separator_detected = ",";
+
+
+    my $data = undef;
+    if ($type_detected eq 'csv') {
+        $data = read_csv($fn, $ordered_hash_available, $separator_detected, $trim_whitespaces);
+    } elsif ($type_detected eq 'table') {
+        $data = read_table($fn, $ordered_hash_available, $trim_whitespaces);
+    } elsif ($type_detected eq 'xlsx') {
+        $data = read_xlsx($fn, $ordered_hash_available);
+    }
+    unless (defined $data) {
+        die "can't read $type_detected format from [$fn]";
+    }
+    return ($data, $type_detected);
+}
+
+sub smart_file_type_detection {
+    my ($file, $xslx_format_available) = @_;
+
+    ## FIX doen't work with STDIN
+    open(my $fh, "<", $file) or die "can't read [$file]: $!";
+    my $lines = "";
+    my $count = 5;
+    while (<$fh>) {
+        $lines .= $_;
+        last unless -- $count;
+    }
+    close($fh);
+
+    if ($lines =~ m/^\s*[{\]]/) { ## "[" or "{"
+        return ('json');
+    } else {
+        my ($first_line, $second_line, undef) = split(m{\r?\n}, $lines, 3);
+
+        if (split(",", $first_line) == split(",", $second_line)) {
+            return ('csv', ",");
+        }
+    }
+    return;
+}
+
+sub write_json {
+    my ($data, $output) = @_;
+
+    print $output to_json(
+        $data,
+        {
+            'utf8' => 1,
+            'pretty' => 1,
+        }
+    );
+}
+
+sub read_csv {
+    my ($fn, $ordered_hash_available, $separator, $trim_whitespaces) = @_;
+
+    my $csv = Text::CSV->new(
+        {
+            'binary' => 1,
+            'sep_char' => $separator,
+            'allow_whitespace' => $trim_whitespaces,
+        }
+    ) or die "can't create Text::CSV: ".Text::CSV->error_diag();
+
+    open(my $fh, "<", $fn) or die "can't read [$fn]: $!";
+    binmode($fh, ":utf8");
+    my ($columns, @rows);
+    while ( my $row = $csv->getline( $fh ) ) {
+        if (defined $columns) {
+            my %r;
+            if ($ordered_hash_available) {
+                tie %r, "Tie::IxHash";
+            }
+            @r{@$columns} = @$row;
+            push(@rows, \%r);
+        } else {
+            $columns = $row;
+        }
+    }
+
+    close($fh);
+
+    return \@rows;
 }
